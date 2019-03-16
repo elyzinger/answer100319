4bc703f1c838319429b4416a443288676d75130e
elyzinger <45950112+elyzinger@users.noreply.github.com>
2019-03-16 16:26:47 +0200

    Add files via upload

---
+++ 1003HW/Book.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1003HW
+{
+    class Book : IComparable<Book>
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+        public string Category { get; set; }
+
+        public Book(string title, string content, string author, string category)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+            Category = category;
+        }
+
+        public override string ToString()
+        {
+            return $"Book Title {Title} : Author : {Author}  Category : "+
+                $"{ Category} Content: { Content} ";
+        }
+
+        /// <summary>
+        /// Sort by author name
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public int CompareTo(Book other)
+        {
+            return Author.CompareTo(other.Author);
+        }
+    }
+}
---
+++ 1003HW/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1003HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyLibrary library = new MyLibrary();
+
+            library.AddBook(
+                new Book("Harry Potter", "But I haven't gotten any messages. From any of my...",
+                "J.K. Rowling", "Fiction"));
+            library.AddBook(
+                new Book("War and Peace", "But mon cher ami, how can you be so calm? This monster, Bonaparte, he's crossed into Austria now, and who's to say that Russia won't be next? No, no, no...",
+                "LEO TOLSTOY", "History"));
+
+            Console.WriteLine(library);
+
+        }
+    }
+}
